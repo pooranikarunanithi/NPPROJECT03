10e180cdfcf22aeac767679115e20af23455ee85
Poorani Karunanithi <Pooranikarunanithi09@gmail.com>
2022-02-09 14:03:54 +0100

    remove api

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 
 export const register = async (user) =>
-  await axios.post(`${process.env.REACT_APP_API}/register`, user);
+ await axios.post("/register", user);
 
   export const login = async (user) =>
-  await axios.post(`${process.env.REACT_APP_API}/login`, user);
+ await axios.post(`${process.env.REACT_APP_API}/login`, user);
 
  // update user in local storage
 export const updateUserInLocalStorage = (user, next) => {
