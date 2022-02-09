8e2c1d07ac3943a7885581dd267917f1b8527201
Poorani Karunanithi <Pooranikarunanithi09@gmail.com>
2022-02-09 14:09:44 +0100

    api

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const register = async (user) =>
- await axios.post("/register", user);
+ await axios.post("api/register", user);
 
   export const login = async (user) =>
  await axios.post(`${process.env.REACT_APP_API}/login`, user);
